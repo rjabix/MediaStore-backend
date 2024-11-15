1b9049a2d00e1f0c4aea4547c30fa7b7ac33b5d3
rjabix <vladislav17052006@gmail.com>
2024-11-15 23:07:37 +0100

    product service simplification

--- ProductMService/Services/ProductService.cs
+++ ProductMService/Services/ProductService.cs
@@ -1,9 +1,8 @@
 ﻿using ProductMService.Models;
-using ProductMService.Models.Categories;
 using Microsoft.EntityFrameworkCore;
-using SQLitePCL;
-using System.Globalization;
-using System.Xml.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ProductMService.Services
 {
@@ -16,110 +15,61 @@ namespace ProductMService.Services
             _context = context;
         }
 
-        public async Task<List<object>?> GetProductByCategory(string category, int page = 0)
+        public async Task<List<Product>?> GetProductsByCategory(string category, int page = 0)
         {
-            var property = _context.GetType().GetProperty(category) ?? throw new ArgumentException("Entity name is not valid", nameof(category));
-
-            if (property.GetValue(_context) is not IQueryable<object> dbSet)
+            var dbSet = GetDbSet(category);
+            var products = await dbSet.Skip(page * 20).ToListAsync();
+            foreach (var product in products)
             {
-                throw new InvalidOperationException("DbSet is not found");
+                product.description = null;
             }
-
-            return await dbSet.Skip(page*20).ToListAsync();
+            return products;
         }
 
         public async Task<object?> GetProductByCategoryAndIdAsync(string category, int id)
         {
-            var property = _context.GetType().GetProperty(category) ?? throw new ArgumentException("Entity name is not valid", nameof(category));
-
-            if (property.GetValue(_context) is not IQueryable<Product> dbSet)
-            {
-                throw new InvalidOperationException("DbSet is not found");
-            }
-
+            var dbSet = GetDbSet(category);
             var product = await dbSet.FirstOrDefaultAsync(i => i.id == id);
-
-            if (product == null)
-            {
-                return null;
-            }
-
-            product.description = null;
+            if (product != null) product.description = null;
             return product;
         }
 
-        public async Task<string?> GetProduct_Description_ByCategoryAndIdAsync(string category, int id)
+        public async Task<string?> GetProductDescriptionByCategoryAndIdAsync(string category, int id)
         {
-            var property = _context.GetType().GetProperty(category) ?? throw new ArgumentException("Entity name is not valid", nameof(category));
-
-            if (property.GetValue(_context) is not IQueryable<Product> dbSet)
-            {
-                throw new InvalidOperationException("DbSet is not found");
-            }
-
-            string? description = await dbSet.Where(i => i.id == id).Select(i => i.description).FirstOrDefaultAsync();
-            return description;
+            var dbSet = GetDbSet(category);
+            return await dbSet.Where(i => i.id == id).Select(i => i.description).FirstOrDefaultAsync();
         }
 
         public async Task<List<object>?> GetPopularProductsAsync()
         {
-            List<object> popularProducts = new List<object>();
+            var popularProducts = new List<object>();
             foreach (var category in Enum.GetNames(typeof(ProductCategory)))
             {
                 try
                 {
-                    var property = _context.GetType().GetProperty(category) ?? throw new ArgumentException("Entity name is not valid", nameof(category));
-
-                    if (property.GetValue(_context) is not IQueryable<Product> dbSet)
-                    {
-                        continue;
-                    }
-                    dbSet = dbSet.Where(p => p.specialTags != null && p.specialTags.Contains("Popular"));
+                    var dbSet = GetDbSet(category).Where(p => p.specialTags != null && p.specialTags.Contains("Popular"));
                     popularProducts.AddRange(await dbSet.ToListAsync());
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                    continue;
-                }
-
+                catch { }
             }
-
             return popularProducts;
         }
 
-        //public async Task<BigPromoItem> AddProductAsync(BigPromoItem item)
-        //{
-        //    _context.BigPromoItems.Add(item);
-        //    await _context.SaveChangesAsync();
-        //    return item;
-        //}
-        //public async Task<List<Product>> GetAllProductsAsync()
-        //{
-        //    return await _context.BigPromoItems.ToListAsync();
-        //}
+        public async Task UpdateProductAsync(Product product)
+        {
+            var dbSet = GetDbSet(product.category.ToCategoryString());
+            var item = await dbSet.FirstOrDefaultAsync(i => i.id == product.id);
+            if (item == null) throw new InvalidOperationException("Product is not found");
+            _context.Entry(item).CurrentValues.SetValues(product);
+            await _context.SaveChangesAsync();
+        }
 
-        //public async Task<BigPromoItem> GetProductByIdAsync(int id)
-        //{
-        //    return await _context.BigPromoItems.FirstOrDefaultAsync(i => i.id == id);
-        //}
-
-        //public async Task UpdateProductAsync(BigPromoItem item)
-        //{
-        //    _context.Entry(item).State = EntityState.Modified;
-        //    await _context.SaveChangesAsync();
-        //}
-
-        //public async Task DeleteProductAsync(int id)
-        //{
-        //    var item = await _context.BigPromoItems.FindAsync(id);
-        //    if (item != null)
-        //    {
-        //        _context.BigPromoItems.Remove(item);
-        //        await _context.SaveChangesAsync();
-        //    }
-        //}
-
+        private IQueryable<Product> GetDbSet(string category)
+        {
+            var property = _context.GetType().GetProperty(category);
+            if (property?.GetValue(_context) is not IQueryable<Product> dbSet)
+                throw new InvalidOperationException("DbSet is not found");
+            return dbSet;
+        }
     }
 }
-
